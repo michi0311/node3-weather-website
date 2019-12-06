3bad7387cf8233e15c7a2909a3528c6e21130d9c
Michael <michael@marolt.com>
2019-12-06 09:24:10 +0100

    test 2

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,5 @@
 //test
-const weatherForm = document.querySelector('form')
+const weatherForm = document.querySelector('form') 
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-one')
 const messageTwo = document.querySelector('#message-two')
